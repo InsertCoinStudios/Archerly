261057eac0e6c7d146e1f3a553f5502d823c7f14
EdgeLordKirito <Leiteritz.till@gmail.com>
2026-01-14 16:06:30 +0100

    worked on these endpoints

--- src/archerly.api/endpoints/LoginEndpoints.cs
+++ src/archerly.api/endpoints/LoginEndpoints.cs
@@ -1,20 +1,36 @@
+using System.Threading.Tasks;
+
 namespace archerly.api.endpoints;
 
 public static class LoginEndpoint
 {
     public static void MapLoginEndpoints(this IEndpointRouteBuilder app)
     {
-        // Contract: JWT or ApiError
         app.MapPost("/login", PostLogin);
     }
 
-    private static IResult PostLogin()
+    private static async Task<IResult> PostLogin(LoginRequest request, Supabase.Client client)
     {
-        // TODO: Login
-        // Retrieve credentials
-        // Provide Credentials to Supabase
-        // Get Supabase Response
-        // Return result to caller
-        return Results.Ok();
+        var session = await client.Auth.SignIn(request.Email, request.Password);
+        if (session is null)
+        {
+            return Results.Unauthorized();
+        }
+        var jwt = session.AccessToken;
+        if (jwt is null)
+        {
+            return Results.Unauthorized();
+        }
+        // TODO: Query Database for if the UserId is Admin
+        bool isAdmin = false;
+        var response = new LoginResponse(
+            jwt,
+            isAdmin,
+            new ExpirationData(session.ExpiresIn, session.ExpiresAt())
+            );
+        return Results.Ok(response);
     }
 }
+public record LoginRequest(string Email, string Password);
+public record LoginResponse(string JWT, bool IsAdmin, ExpirationData Expiration);
+public record ExpirationData(long ExpiresInSec, DateTime ExpiresAt);
--- src/archerly.api/endpoints/RegisterEndpoint.cs
+++ src/archerly.api/endpoints/RegisterEndpoint.cs
@@ -1,16 +1,20 @@
 
 namespace archerly.api.endpoints;
 
+// TODO: In Supabase set Confirm Email and Confirm Phonenumber to off
+// if this is not done it will be a lot harder for the backend
 public static class RegisterEndpoint
 {
     public static void MapRegisterEndpoints(this IEndpointRouteBuilder app)
     {
-        // Contract: JWT or ApiError
         app.MapPost("/register", PostRegister);
     }
-    private static IResult PostRegister()
+    private static IResult PostRegister(RegisterRequest request, Supabase.Client client)
     {
         // TODO: Register
         return Results.Ok();
     }
 }
+
+public record RegisterRequest(string Email, string Password);
+public record RegisterResponse(bool Success, string? ErrorMessage);
