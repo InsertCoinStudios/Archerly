7febe014d4ec905b7e3966cadf530690b27923c4
EdgeLordKirito <Leiteritz.till@gmail.com>
2026-01-20 11:32:48 +0100

    added from function

--- src/archerly.entities/Shot.cs
+++ src/archerly.entities/Shot.cs
@@ -27,4 +27,19 @@ public class Shot
 
     //    [Column("huntid")]
     public Guid HuntId { get; set; }
+
+    public static Shot From(Guid id, Guid user_id, Guid animal_id, long score, int kind, long shotNumber, Guid huntId)
+    {
+        var shot = new Shot
+        {
+            Id = id,
+            UserId = user_id,
+            AnimalId = animal_id,
+            Score = score,
+            Kind = kind,
+            ShotNumber = shotNumber,
+            HuntId = huntId,
+        };
+        return shot;
+    }
 }
